54ca73985982a91b016406bf347a4dab7b657130
B1boid <amigodos@yandex.ru>
2019-09-28 23:17:14 +0300

    user location bug fixed

--- IRLKiller/MainViewController.swift
+++ IRLKiller/MainViewController.swift
@@ -13,6 +13,9 @@ class MainViewController: UIViewController,MGLMapViewDelegate {
     
     @IBOutlet weak var testText: UILabel!
     
+    @IBAction func clickMyLocation(_ sender: Any) {
+        showMyLocation()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,35 +39,43 @@ class MainViewController: UIViewController,MGLMapViewDelegate {
         
     }
     
+    var mapView:MGLMapView!
+    
     func setUpMapView(){
         // доабавляю программно
-        let mapView = MGLMapView(frame: view.bounds, styleURL: URL(string:"mapbox://styles/b1boid/ck12egt7e02ax1cp52uginkky"))
+        mapView = MGLMapView(frame: view.bounds, styleURL: URL(string:"mapbox://styles/b1boid/ck12egt7e02ax1cp52uginkky"))
         
         // Чтобы не видно было надписи mapbox внизу - смещаю
         mapView.frame = CGRect(x: 0, y: 60, width: view.bounds.maxX, height: view.bounds.maxY)
-        
-        
-        //mapView.setCenter(CLLocationCoordinate2D(latitude: 40.74699, longitude: -73.98742), zoomLevel: 10, animated: false)
-        
-        view.addSubview(mapView)
         
         // Set the map view's delegate
         mapView.delegate = self
         
         // Allow the map view to display the user's location
         mapView.showsUserLocation = true
+
+        view.addSubview(mapView)
     }
     
     
     func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
         // Wait for the map to load before initiating the first camera movement.
-        
+
         // Create a camera that rotates around the same center point, rotating 180°.
         // `fromDistance:` is meters above mean sea level that an eye would have to be in order to see what the map view is showing.
-        let camera = MGLMapCamera(lookingAtCenter: mapView.userLocation?.coordinate ?? CLLocationCoordinate2D(latitude: 0, longitude: 0), altitude: 500, pitch: 30, heading: 180)
         
-        // Animate the camera movement over 2 seconds.
-        mapView.setCamera(camera, withDuration: 2, animationTimingFunction: CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut))
+        //Вообщем есть необьяснимый баг - при запуске программы когда эмулятор закрыт, то есть когда эмулятор открывается и сразу запускается приложение - иногда не находит координаты пользователя сразу, поэтому вводим вспомогательную камеру на фиксированную точку(в будущем на последнее местопложение пользователя)
+        let camera = MGLMapCamera(lookingAtCenter:CLLocationCoordinate2D(latitude: 40.74699, longitude: -73.98742), altitude: 500, pitch: 30, heading: 180)
+        mapView.setCamera(camera, animated: false)
+        
+        let camera2 = MGLMapCamera(lookingAtCenter: mapView.userLocation?.coordinate ?? CLLocationCoordinate2D(latitude: 40.74699, longitude: -73.98742), altitude: 500, pitch: 30, heading: 180)
+        mapView.setCamera(camera2, animated: false)
+        
+    }
+    
+    func showMyLocation(){
+        let camera2 = MGLMapCamera(lookingAtCenter: mapView.userLocation?.coordinate ?? CLLocationCoordinate2D(latitude: 40.74699, longitude: -73.98742), altitude: 500, pitch: 30, heading: 180)
+        mapView.setCamera(camera2, animated: false)
     }
     
 
